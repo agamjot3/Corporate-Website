ebf2642ef69ae07ba8e54431b2973fb72eeb72fb
agamjot3 <agamjot3@gmail.com>
2020-09-28 18:50:57 +0530

    Last Commit

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $('#navbar a, .btn').on('click', function(event){
 
         $('html, body').animate(
             {
-                scrollTop: $(hash).offset().top -100
+                scrollTop: $(hash).offset().top -10
             },
             800
         );
